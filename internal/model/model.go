f37e8a3063d51248533b1c6bd83db297166c02a8
zeroDouBobaFett <vasylko2005@gmail.com>
2025-04-11 03:05:22 +0300

    fix db migration

--- internal/model/model.go
+++ internal/model/model.go
@@ -7,14 +7,14 @@ import (
 )
 
 type User struct {
-	ID       *uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey;unique"`
+	ID       *uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Username *string    `gorm:"type:varchar(100);not null;unique"`
 	Password *string    `gorm:"type:char(64);not null"`
 	Messages *[]Message `gorm:"foreignKey:UserID"`
 }
 
 type Message struct {
-	ID              *uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey;unique"`
+	ID              *uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Text            *string    `gorm:"type:text"`
 	Timestamp       *time.Time `gorm:"type:timestamp;default:current_timestamp"`
 	ExpirationDate  *time.Time `gorm:"type:timestamp;default:null"`
